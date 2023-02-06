3697dbe594414e4b430ec504ae3fb84907434b6c
kristofeeerr <123837931+kristofeeerr@users.noreply.github.com>
2023-02-06 11:48:00 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesCount = 52;
+            int numberPerRows = 3;
+            int filledRows = picturesCount / numberPerRows;
+            int surplusPictures = picturesCount % numberPerRows;
+            Console.WriteLine($"Полностью заполненных рядов - {filledRows}. Избыток картинок - {surplusPictures}");
+        }
+    }
+}
